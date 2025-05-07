c441e7844d95e8827cadedb0b05e8201fea66594
Biniyam Ambachew <101281173+bini34@users.noreply.github.com>
2025-05-07 13:12:48 +0300

    feat(core): add core functionality including routing, theme, and error handling
    This commit introduces core functionality to the application, including:
    - Routing setup with GoRouter for authentication-related pages
    - Theme configuration with light and dark modes
    - Basic error handling structures (failures and exceptions)
    These changes lay the foundation for further feature development and ensure a consistent user experience.

---
+++ client/lib/core/routes/router.dart
@@ -0,0 +1,9 @@
+import 'package:go_router/go_router.dart';
+import 'package:MediTrack/core/routes/auth_routes.dart';
+
+final GoRouter router = GoRouter(
+  routes: [
+  
+    ...authRoutes,
+  ],
+);
